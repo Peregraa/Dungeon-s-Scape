ea1e837e19ef478f816ec5442fa54b5976a1de84
BlueDragon <tayrickbutinhol@gmail.com>
2018-10-26 15:17:36 -0300

    Versão 1.0
    Movimento do herói Ban + animação

---
+++ Dungeon Scape Versões/Beta (Movimentação completa + animação herói)/Dungeon Scape 0.1/Assets/Scripts/SeguirCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguirCamera : MonoBehaviour {
+
+
+    private Vector2 velocity;
+
+
+    //Onde a smoothTime camera vai se movimentar
+    public float smoothTimeY;
+    public float smoothTimeX;
+
+
+    public GameObject player;
+
+	void Start () {
+        //Acha o came objetc com a tag ''Player'', que é o jogador em si
+        player = GameObject.FindGameObjectWithTag("Player");
+
+	}
+    void FixedUpdate()
+    {
+        //Transfere a posição x e y para o jogador
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}

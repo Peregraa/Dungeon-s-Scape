fb1de4e6ff565b73e11cd92f1cdb790f15945336
Vitor <vitor.mendonca95@gmail.com>
2018-11-02 15:40:44 -0200

    Sprint 2
    Cenário, armadilhas, sistema de vida e checkpoint

---
+++ Dungeon Scape Versões/Dungeon Scape 0.1/Assets/Scripts/Player.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+
+public class Player:MonoBehaviour {
+    
+    //Var para realizar validação do pulo 
+    bool isJumping = false; 
+    bool isOnFloor = false; 
+    private int maxJump;
+
+    //vida
+    public int vida = 3;
+
+
+    //Checkpoint
+    public GameObject lastCheckPoint;
+
+    //Var para velocidade do herói e força do pulo
+    public float jumpForce = 550f; 
+    public float speed = 5f; 
+    public float radius = 0.35f; 
+
+    //Var validando o chão
+    public Transform groundCheck; 
+    public LayerMask whatIsGround; 
+    
+    Rigidbody2D body; 
+    SpriteRenderer sprite; 
+    Animator anime; 
+
+    // Use this for initialization
+    void Start() {
+        body = GetComponent < Rigidbody2D > (); 
+        sprite = GetComponent < SpriteRenderer > (); 
+        anime = GetComponent < Animator > (); 
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //Verifica se o herói está no chão
+        isOnFloor = Physics2D.OverlapCircle(groundCheck.position, radius, whatIsGround); 
+        
+        //Validando pulo e duplo pulo
+        if (Input.GetButtonDown("Jump") && maxJump > 0)
+            
+            isJumping = true; 
+
+        //Herói toca no chão e ganha pulo
+        if (isOnFloor) {
+            maxJump = 1; 
+        }
+
+        //Carregando a animação ao iniciar o jogo
+        PlayerAnimation(); 
+
+    }
+
+    void FixedUpdate() {
+        //MOVE - recebe quando as teclas <- -> são pressionadas
+        float move = Input.GetAxis("Horizontal"); 
+        //Velocidade e movimento do herói tanto no ar quanto no solo 
+        body.velocity = new Vector2(move * speed, body.velocity.y); 
+
+        if ((move > 0 && sprite.flipX == true) || (move < 0 && sprite.flipX == false)) {
+            Flip(); 
+        }
+
+        //Ação de pulo
+        if (isJumping) {
+            maxJump--; 
+            body.velocity = new Vector2(body.velocity.x, 0f); 
+            body.AddForce(new Vector2(0f, jumpForce)); 
+            isJumping = false; 
+        }
+    }
+
+    void Flip() {
+        sprite.flipX =  ! sprite.flipX; 
+    }
+
+    //Aumentando o alcance do groundCheck para evitar erros no pulo.
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red; 
+        Gizmos.DrawWireSphere(groundCheck.position, radius); 
+    }
+
+    //Validando animação do herói
+    void PlayerAnimation() {
+        anime.SetFloat("VelX", Mathf.Abs (body.velocity.x)); 
+        anime.SetFloat("VelY", Mathf.Abs (body.velocity.y)); 
+    }
+
+
+
+
+
+    //Fazendo morte e checkpoint do personagem
+    void OnCollisionEnter2D(Collision2D collision2d) {
+        if (collision2d.gameObject.CompareTag("Espeto")) {
+            vida--;
+            //anime.SetTrigger("Morreu");
+            transform.position = lastCheckPoint.transform.position;
+
+            
+            if (vida == 0) {
+                //GameOver
+            }
+        }
+
+          //Quando criar os inimigos, colocar a tag "Inimigo" neles.
+        //if (collision2d.gameObject.CompareTag("Inimigo")) {
+          //  vida--; 
+            //anime.SetTrigger("Morreu"); 
+
+
+            
+       //     if (vida == 0) {
+                //GameOver
+         //   }
+      //  }
+
+    }
+
+        void OnTriggerEnter2D(Collider2D collision2d){
+        // Fazendo Checkpoint
+        if (collision2d.gameObject.CompareTag("checkpoint")){
+        
+                
+                lastCheckPoint = collision2d.gameObject;
+        }
+        
+    }
+
+}

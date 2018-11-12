1f9280f5026ab0957e08994cf4a512f6e7e173f5
rafaelrochaaazvedo <rafaelrochaazvedo@gmail.com>
2018-11-12 21:21:03 -0200

    Update  Cenario
    Atualizações e Incrementações do mapa

--- Dungeon Scape Versões/Dungeon Scape 0.1/Assets/Scripts/Player.cs
+++ Dungeon Scape Versões/Dungeon Scape 0.1/Assets/Scripts/Player.cs
@@ -9,22 +9,29 @@ public class Player:MonoBehaviour {
     bool isOnFloor = false; 
     private int maxJump;
 
+    //Agaichar
+    bool duck = false;
+   
     //vida
     public int vida = 3;
-
-
     //Checkpoint
     public GameObject lastCheckPoint;
 
+    [Header("Movement Variables")]
     //Var para velocidade do herói e força do pulo
     public float jumpForce = 550f; 
     public float speed = 5f; 
     public float radius = 0.35f; 
-
     //Var validando o chão
     public Transform groundCheck; 
-    public LayerMask whatIsGround; 
-    
+    public LayerMask whatIsGround;
+
+    [Header("Attack Variables")]
+    public Transform attackCheck;
+    public float radiusAttack;
+    public LayerMask layerEnemy;
+    float timeNextAttack;
+
     Rigidbody2D body; 
     SpriteRenderer sprite; 
     Animator anime; 
@@ -42,25 +49,55 @@ public class Player:MonoBehaviour {
         isOnFloor = Physics2D.OverlapCircle(groundCheck.position, radius, whatIsGround); 
         
         //Validando pulo e duplo pulo
-        if (Input.GetButtonDown("Jump") && maxJump > 0)
-            
+        //if (Input.GetButtonDown("Jump") && maxJump > 0) 
+		if(Input.GetKeyDown(KeyCode.UpArrow) && maxJump > 0 && duck == false )
             isJumping = true; 
+      
 
-        //Herói toca no chão e ganha pulo
+        //Herói toca no chão e ganha pulo - EVITA que o HEROI voe
         if (isOnFloor) {
-            maxJump = 1; 
+            maxJump = 1;
         }
 
+
+        //Attack do personagem - validando quando atacar
+        if (timeNextAttack <= 0f && duck == false)
+        {
+            //if (Input.GetButtonDown("Fire1") && body.velocity == new Vector2(0, 0))
+            if (Input.GetKey(KeyCode.Space) && body.velocity == new Vector2(0, 0))
+            {
+                anime.SetTrigger("Attack");
+                timeNextAttack = 0.2f;
+            }
+        }
+        else {
+            timeNextAttack -= Time.deltaTime;
+        }
+
+
+        //Correr
+        if (Input.GetKey(KeyCode.LeftShift) && speed < 7) {
+            speed += 0.1f;
+        } else if (!Input.GetKey(KeyCode.LeftShift) && speed > 5){
+            speed -= 0.1f;
+        }
+
+
         //Carregando a animação ao iniciar o jogo
-        PlayerAnimation(); 
+        PlayerAnimation();
 
     }
 
     void FixedUpdate() {
         //MOVE - recebe quando as teclas <- -> são pressionadas
-        float move = Input.GetAxis("Horizontal"); 
-        //Velocidade e movimento do herói tanto no ar quanto no solo 
-        body.velocity = new Vector2(move * speed, body.velocity.y); 
+        float move = Input.GetAxis("Horizontal");
+
+
+        //Impedindo que o Herói anda agaichado
+        if (duck == false) {
+            //Velocidade e movimento do herói tanto no ar quanto no solo 
+            body.velocity = new Vector2(move * speed, body.velocity.y);
+        }
 
         if ((move > 0 && sprite.flipX == true) || (move < 0 && sprite.flipX == false)) {
             Flip(); 
@@ -73,16 +110,41 @@ public class Player:MonoBehaviour {
             body.AddForce(new Vector2(0f, jumpForce)); 
             isJumping = false; 
         }
+
+        //Fazendo personagem abaixar
+       
+        if  (Input.GetAxis("Vertical") < 0)
+        {
+            duck = true;
+        }
+        else
+        {
+            duck = false;
+        }
+        anime.SetBool("Duck", duck);
     }
 
     void Flip() {
-        sprite.flipX =  ! sprite.flipX; 
+        sprite.flipX =  ! sprite.flipX;
+        //Direcionando o lado do ataque com a espada.
+        attackCheck.position = new Vector2(-attackCheck.localPosition.x, attackCheck.localPosition.y);
     }
+
+    void PlayerAttack() {
+        //Atacar mais de um inimigo
+        Collider2D[] enemiesAttack = Physics2D.OverlapCircleAll(attackCheck.position, radiusAttack, layerEnemy);
+        for (int i = 0; i < enemiesAttack.Length; i++){
+            enemiesAttack[i].SendMessage("EnemyHit");
+            Debug.Log(enemiesAttack[i].name);
+        }
+    }
+
 
     //Aumentando o alcance do groundCheck para evitar erros no pulo.
     void OnDrawGizmosSelected() {
         Gizmos.color = Color.red; 
         Gizmos.DrawWireSphere(groundCheck.position, radius); 
+        Gizmos.DrawWireSphere(attackCheck.position, radiusAttack);
     }
 
     //Validando animação do herói
